ad73f3fa66169fdcc8d07b0d103653e3fa5794ff
Juan_D <jdvc96@gmail.com>
2025-01-14 21:13:33 -0300

    Ajustes visuales de CSS

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,13 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-Post.create(title: "Título de prueba", content: "Contenido de prueba", user_id: 1)
+Post.destroy_all
+User.destroy_all
+
+# Crear usuarios con diferentes roles
+user1 = User.create(email: 'user1@example.com', password: 'password', role: 0)
+user2 = User.create(email: 'user2@example.com', password: 'password', role: 1)
+user3 = User.create(email: 'user3@example.com', password: 'password', role: 2)
+
+# Crear posts para cada usuario
+Post.create(title: 'Post de Usuario 1', content: 'Este es el primer post, creado por el Usuario 1.', user: user1)
+Post.create(title: 'Post de Usuario 2', content: 'Este es el segundo post, creado por el Usuario 2.', user: user2)
+Post.create(title: 'Post de Usuario 3', content: 'Este es el tercer post, creado por el Usuario 3.', user: user3)
+
